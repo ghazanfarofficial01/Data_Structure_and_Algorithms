0f589d08ff8c0cbc3c5f12e95fcb7dff6d3fb5c9
ghazanfarofficial01 <56466981+ghazanfarofficial01@users.noreply.github.com>
2022-04-12 05:39:14 +0530

    Update Find_Missing_And_Repeating.java
    Added solution which takes only O(1) extra space.

--- Find_Missing_And_Repeating.java
+++ Find_Missing_And_Repeating.java
@@ -27,3 +27,30 @@ class Solve {
 
 //Time- O(N)
 //Space - O(N)
+
+
+/* Another approach for O(N) time and O(1)
+extra space*/
+
+class Solve {
+    int[] findTwoElement(int arr[], int n) {
+        // code here
+        int[] result = new int[2];
+        
+        //find twice occuring
+        for(int i = 0; i<n; i++){
+            int abs_value = Math.abs(arr[i]);
+            if(arr[abs_value -1] > 0){
+                arr[abs_value-1] = -arr[abs_value-1];
+            }
+            else result[0] = abs_value;
+        }
+            //finding missing
+            for(int i = 0; i<n; i++){
+                if(arr[i] > 0) result[1] = i+1;
+            }
+            
+            return result;
+        
+    }
+}
