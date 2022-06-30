3dfe3ab356e409c3eb14bd86500c8cb68d763c3e
ghazanfarofficial01 <56466981+ghazanfarofficial01@users.noreply.github.com>
2022-06-30 18:31:03 +0530

    https://practice.geeksforgeeks.org/problems/remove-duplicate-element-from-sorted-linked-list/1#

---
+++ Remove duplicate element from sorted Linked List .java
@@ -0,0 +1,12 @@
+ Node current = head;
+ while(current.next != null){
+     if(current.data == current.next.data){
+         current.next = current.next.next;
+     }
+     else{
+         current = current.next;
+     }
+ }
+	return head;
+    }
+}
