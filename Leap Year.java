8c14c276dc871b8d918b0ffa5649d4174f66ed43
ghazanfarofficial01 <56466981+ghazanfarofficial01@users.noreply.github.com>
2022-07-30 14:35:58 +0530

    Create Leap Year.java

---
+++ Leap Year.java
@@ -0,0 +1,7 @@
+class Solution{
+    static int isLeap(int N){
+        //code here
+        if((N % 4 == 0 && N % 100 != 0) || (N % 100 == 0 && N % 400 == 0)) return 1;
+        else return 0;
+    }
+}
