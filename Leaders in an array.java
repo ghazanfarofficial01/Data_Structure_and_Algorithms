43c0cb518a2d892d0ed65ca6b30ff38983eac81c
ghazanfarofficial01 <56466981+ghazanfarofficial01@users.noreply.github.com>
2022-08-08 20:35:45 +0530

    Create Leaders in an array.java
    link - https://practice.geeksforgeeks.org/problems/leaders-in-an-array-1587115620/1

---
+++ Leaders in an array.java
@@ -0,0 +1,18 @@
+class Solution{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(arr[n-1]);
+        int max = arr[n-1];
+        for(int i = n-2; i>=0; i--){
+            if(arr[i]>=max){
+                max = arr[i];
+                list.add(max);
+            }
+            else continue;
+        }
+        Collections.reverse(list);
+        return list;
+    }
+}
