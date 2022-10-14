daffba86042066320783989e9d7e4a63a7951001
ghazanfarofficial01 <56466981+ghazanfarofficial01@users.noreply.github.com>
2022-10-14 16:51:46 +0530

    Create Encrypt the string - 2.java
    link - https://practice.geeksforgeeks.org/problems/encrypt-the-string-21117/1?page=2&difficulty[]=0&category[]=Strings&curated[]=7&curated[]=3&sortBy=submissions

---
+++ Encrypt the string - 2.java
@@ -0,0 +1,66 @@
+class Solution {
+    static String encryptString(String S){
+        // code here
+        StringBuilder ans = new StringBuilder();
+         int N = S.length();
+        // Iterate the characters
+        // of the string
+        for (int i = 0; i < N; i++) {
+ 
+            char ch = S.charAt(i);
+            int count = 0;
+            String hex;
+ 
+            // Iterate until S[i] is equal to ch
+            while (i < N && S.charAt(i) == ch) {
+ 
+                // Update count and i
+                count++;
+                i++;
+            }
+ 
+            // Decrement i by 1
+            i--;
+ 
+            // Convert count to hexadecimal
+            // representation
+            hex = convertToHex(count);
+ 
+            // Append the character
+            ans.append(ch);
+ 
+            // Append the characters frequency
+            // in hexadecimal representation
+            ans.append(hex);
+        }
+ 
+        // Reverse the obtained answer
+        ans.reverse();
+ 
+        // Return required answer
+        return ans.toString();
+    }
+
+
+ // Function to convert Decimal to Hex
+    static String convertToHex(int num)
+    {
+ 
+        StringBuilder temp = new StringBuilder();
+        while (num != 0) {
+            int rem = num % 16;
+            char c;
+            if (rem < 10) {
+                c = (char) (rem + 48);
+            }
+            else {
+                c = (char) (rem + 87);
+            }
+            temp.append(c);
+            num = num / 16;
+        }
+ 
+        return temp.toString();
+    }
+ 
+}
