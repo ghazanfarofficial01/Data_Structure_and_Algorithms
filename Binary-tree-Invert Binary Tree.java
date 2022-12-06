8a2b2212e7d6fffcd571c5519fe5a39f3ec0b12c
ghazanfarofficial01 <56466981+ghazanfarofficial01@users.noreply.github.com>
2022-12-06 21:41:37 +0530

    Create Binary-tree-Invert Binary Tree.java
    https://leetcode.com/problems/invert-binary-tree/description/

---
+++ Binary-tree-Invert Binary Tree.java
@@ -0,0 +1,17 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        TreeNode temp = left;
+        root.left = right;
+        root.right = temp;
+
+        return root; 
+
+
+
+    }
+}
